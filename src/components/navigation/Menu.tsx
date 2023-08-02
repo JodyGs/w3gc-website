e1a2a024e995c001f64f894bb5c3dbbe0cc481b0
JodyGs <demajn@tuta.io>
2023-08-02 14:16:15 +0200

    anchor + smooth  scroll

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LogoNav, RightArrow } from "../svg/svg";
 import Link from "next/link";
 import { NAVIGATION } from "~/utils/constants";
+import AnchorLink from "react-anchor-link-smooth-scroll-v2";
 
 export default function Menu() {
   return (
@@ -12,12 +13,12 @@ export default function Menu() {
         </div>
         <nav className="flex items-center">
           <div className="flex items-center justify-center pl-2">
-            <Link
+            <AnchorLink
               href="#home"
               className="rounded-[2px] border-[1px] border-transparent p-2 px-4 py-3 text-[#D4D6DC] transition-all hover:border-[#686A7A] hover:text-white active:border-white active:bg-white active:text-[#2C2D38]"
             >
               <RightArrow className="h-4 w-4 -rotate-90 scale-110 fill-white active:fill-[#2c2d38]" />
-            </Link>
+            </AnchorLink>
           </div>
           <ul className="flex space-x-2 p-2">
             {NAVIGATION.anchor.map((item, index) => (
@@ -25,9 +26,9 @@ export default function Menu() {
                 key={index}
                 className="rounded-[2px] border-[1px] border-transparent px-4 py-3 text-center font-inter text-sm leading-5 text-[#D4D6DC] transition-all hover:border-[#686A7A] hover:text-white active:border-white active:bg-white active:text-[#2C2D38]"
               >
-                <Link href={item.href} className="whitespace-nowrap">
+                <AnchorLink href={item.href} className="whitespace-nowrap">
                   {item.name}
-                </Link>
+                </AnchorLink>
               </li>
             ))}
           </ul>
